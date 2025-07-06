fbb902851e6745cc6f8506beb08198db1bade26c
marcosmc <demiguelm@aia.es>
2025-07-06 17:15:00 +0200

    Update WTG4AWeccCurrentSource to WTG4AWeccCurrentSource1 in model definitions and tests

--- tests/dycov/sanity_checks/test_parameter_checks.py
+++ tests/dycov/sanity_checks/test_parameter_checks.py
@@ -81,7 +81,7 @@ def test_generators():
     )
     ppm = parameters.Gen_params(
         id=None,
-        lib="WTG4AWeccCurrentSource",
+        lib="WTG4AWeccCurrentSource1",
         connectedXmfr="",
         SNom=90,
         IMax=100.0,
