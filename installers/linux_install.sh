236c2d558000b2ed0619f5b2072731492ac24ad1
Jose Luis Marin <jlmarinjl@gmail.com>
2025-11-15 21:28:13 +0100

    format bash script with: shfmt --indent 4 --case-indent --space-redirects

--- installers/linux_install.sh
+++ installers/linux_install.sh
@@ -12,9 +12,9 @@ set -o nounset -o noclobber
 set -o errexit -o pipefail
 
 # Default Configuration Variables
-RELEASE_TAG="v0.8.1"
+RELEASE_TAG="v0.9.1"
 DYNAWO_ZIP_FILE="Dynawo_omc_v1.8.0.zip"
-DYNAWO_CHECKSUM="2e2f36920d729413126ae3dbea94e34e11b6ab33"
+DYNAWO_SHA256SUM="fbba80aa7ac6a990928b601e339a43ec49d538b956c97a51d038d1dcdea48768"
 REPO_URL="https://github.com/dynawo/dyn-grid-compliance-verification.git"
 
 # Script State Variables
@@ -22,13 +22,13 @@ INSTALL_DIR="$PWD/dycov"
 LOG_FILE_NAME="" # Will be set later
 NON_INTERACTIVE=false
 CUSTOM_ZIP_USED=false
-DIRECT_URL="" # Variable for direct source code download.
+DIRECT_URL=""       # Variable for direct source code download.
 INSTALL_DYNAWO=true # Controls the optional installation of Dynawo.
 
 # Helper Functions
-RED="\\033[1;31m"
-GREEN="\\033[1;32m"
-NC="\\033[0m"
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+NC='\033[0m'
 
 # Make a copy of the original stdout for writing to the console later.
 # This must be done before any function uses >&6.
@@ -76,17 +76,17 @@ trap 'error_handler $? $LINENO' ERR
 # Asks for confirmation before deleting, unless --yes mode is active.
 confirm_and_delete() {
     local target="$1"
-    
+
     if [[ "$NON_INTERACTIVE" == true ]]; then
         color_msg "Non-interactive mode: deleting $target without prompting."
         rm -rf "$target"
         return
     fi
-    
+
     local response
     echo -n -e "\n${RED}WARNING:${NC} This will permanently delete: ${target}. Are you sure you want to continue? [y/N] " >&6
     read -r response <&6
-    
+
     case "$response" in
         [yY][eE][sS] | [yY])
             color_msg "User confirmed deletion of: $target. Deleting..."
@@ -121,7 +121,7 @@ find_python_cmd() {
             echo "$($interp --version 2>&1 | awk '{print $2}') $interp"
         done | sort -V -r | head -n 1 | awk '{print $2}')
     fi
-    
+
     python_cmd="$best_interpreter"
 }
 
@@ -142,13 +142,36 @@ usage() {
 while [[ $# -gt 0 ]]; do
     key="$1"
     case $key in
-        -r|--release) RELEASE_TAG="$2"; shift; shift;;
-        -u|--url) DIRECT_URL="$2"; shift; shift;;
-        -z|--zipfile) DYNAWO_ZIP_FILE="$2"; CUSTOM_ZIP_USED=true; shift; shift;;
-        -d|--directory) INSTALL_DIR="$2"; shift; shift;;
-        -y|--yes) NON_INTERACTIVE=true; shift;;
-        -h|--help) usage;;
-        *) echo "Unknown option: $1"; usage;;
+        -r | --release)
+            RELEASE_TAG="$2"
+            shift
+            shift
+            ;;
+        -u | --url)
+            DIRECT_URL="$2"
+            shift
+            shift
+            ;;
+        -z | --zipfile)
+            DYNAWO_ZIP_FILE="$2"
+            CUSTOM_ZIP_USED=true
+            shift
+            shift
+            ;;
+        -d | --directory)
+            INSTALL_DIR="$2"
+            shift
+            shift
+            ;;
+        -y | --yes)
+            NON_INTERACTIVE=true
+            shift
+            ;;
+        -h | --help) usage ;;
+        *)
+            echo "Unknown option: $1"
+            usage
+            ;;
     esac
 done
 
@@ -159,7 +182,6 @@ DATETIME=$(date '+%Y%m%d_%H%M%S')
 LOG_FILE_NAME="installation_$DATETIME.log" # Assign the name here to be used by the error handler
 LOG="$INSTALL_DIR/$LOG_FILE_NAME"
 DYNAWO_ZIP_URL="https://github.com/dynawo/dyn-grid-compliance-verification/releases/download/$RELEASE_TAG/$DYNAWO_ZIP_FILE"
-
 
 #######################################
 # START OF THE INSTALLATION LOGIC
@@ -172,7 +194,7 @@ if [ -d "$INSTALL_DIR" ]; then
 fi
 mkdir -p "$INSTALL_DIR"
 
-exec >"$LOG"
+exec > "$LOG"
 exec 7>&2
 exec 2>&1
 
@@ -213,7 +235,7 @@ cd "$INSTALL_DIR"
 if [[ "$NON_INTERACTIVE" == false ]]; then
     echo -n -e "\nDo you want to download and install Dynawo? (Required for some examples) [Y/n] " >&6
     read -r response <&6
-    
+
     case "$response" in
         [nN][oO] | [nN])
             INSTALL_DYNAWO=false
@@ -237,9 +259,9 @@ if [[ "$INSTALL_DYNAWO" == true ]]; then
         color_msg "NOTICE: Skipping checksum verification for custom Dynawo ZIP file."
     else
         color_msg "Verifying Dynawo ZIP file checksum..."
-        CHECKSUM_CALCULATED=$(shasum "$DYNAWO_ZIP_FILE" | cut -d" " -f1)
-        if [ "$CHECKSUM_CALCULATED" != "$DYNAWO_CHECKSUM" ]; then
-            color_err_msg "FATAL ERROR: Checksum mismatch. Expected: '$DYNAWO_CHECKSUM', Got: '$CHECKSUM_CALCULATED'. Aborting for security."
+        SHA256SUM_CALCULATED=$(sha256sum "$DYNAWO_ZIP_FILE" | cut -d" " -f1)
+        if [ "$SHA256SUM_CALCULATED" != "$DYNAWO_SHA256SUM" ]; then
+            color_err_msg "FATAL ERROR: Checksum mismatch. Expected: '$DYNAWO_SHA256SUM', Got: '$SHA256SUM_CALCULATED'. Aborting for security."
             exit 1
         fi
         color_msg "Checksum verified successfully."
@@ -267,14 +289,14 @@ fi
 
 if [ -n "$DIRECT_URL" ]; then
     color_msg "Step 2: Downloading DyCoV source code from direct URL..."
-    
+
     SOURCE_ZIP_FILENAME="${DIRECT_URL##*/}"
     color_msg "Downloading as: $SOURCE_ZIP_FILENAME"
 
     curl -L --fail "$DIRECT_URL" -o "$SOURCE_ZIP_FILENAME"
     unzip -q "$SOURCE_ZIP_FILENAME"
     rm -f "$SOURCE_ZIP_FILENAME" # Changed to rm -f to avoid errors if it doesn't exist.
-    
+
     UNZIPPED_DIR=$(find . -mindepth 1 -maxdepth 1 -type d ! -name 'dynawo')
     if [ -z "$UNZIPPED_DIR" ] || [ "$(echo "$UNZIPPED_DIR" | wc -l)" -ne 1 ]; then
         color_err_msg "ERROR: Could not determine the unzipped source directory. Expected a single directory."
