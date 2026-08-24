d8693a870684946ed5d128c3d8701a82c14ba564
marcosmc <demiguelm@aia.es>
2026-08-24 11:31:36 +0200

    fix: improve help message formatting and logging in functional testing

--- src/dycov/gfm/tests/functional_tests.py
+++ src/dycov/gfm/tests/functional_tests.py
@@ -1,6 +1,7 @@
-import pandas as pd
 from pathlib import Path
 from typing import Tuple
+
+import pandas as pd
 
 
 def compare_csv_directories(baseline_dir: Path, output_dir: Path) -> Tuple[bool, str]:
