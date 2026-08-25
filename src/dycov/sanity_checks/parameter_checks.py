7b45dcc55fb556195480c6b73af8565e32c894bb
marcosmc <demiguelm@aia.es>
2026-08-25 09:08:47 +0200

    fix(parameter_checks): correct check_trafo function parameter and improve docstring

--- src/dycov/sanity_checks/parameter_checks.py
+++ src/dycov/sanity_checks/parameter_checks.py
@@ -240,8 +240,8 @@ def check_trafos(xfmrs: list[XfmrParams]) -> None:
         check_trafo(xfmr)
 
 
-def check_trafo(xfmrs: XfmrParams) -> None:
-    """Check whether the user-supplied tranformer parameters are consistent:
+def check_trafo(xfmr: XfmrParams) -> None:
+    """Check whether the user-supplied transformer parameters are consistent:
     * The reactance of the transformer must be greater than 0.
 
     Parameters
@@ -249,14 +249,12 @@ def check_trafo(xfmrs: XfmrParams) -> None:
     xfmr: XfmrParams
         Transformer parameters.
     """
-    if xfmrs and xfmrs.x <= 0:
-        raise ValueError(
-            f"The reactance of the transformer {xfmrs.id} must be greater than zero."
-        )
+    if xfmr and xfmr.x <= 0:
+        raise ValueError(f"The reactance of the transformer {xfmr.id} must be greater than zero.")
 
-    if xfmrs and xfmrs.alpha_tfo != 0.0:
+    if xfmr and xfmr.alpha_tfo != 0.0:
         raise ValueError(
-            f"The alphaTfo parameter of the transformer {xfmrs.id} must be equal to zero."
+            f"The alphaTfo parameter of the transformer {xfmr.id} must be equal to zero."
         )
 
 
