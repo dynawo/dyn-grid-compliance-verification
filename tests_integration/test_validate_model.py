d3f78c863534e698b82e871e8b20b82a4d8bac48
marcosmc <demiguelm@aia.es>
2026-08-31 16:44:20 +0200

    fix: update compliance status in model validation tests

--- tests_integration/test_validate_model.py
+++ tests_integration/test_validate_model.py
@@ -26,7 +26,7 @@ def test_model_validation_ppm_dynawo_model():
         Compliance.Compliant,  # 4
         Compliance.Compliant,  # 5
         Compliance.Compliant,  # 6
-        Compliance.NotApplicableTest,  # 7
+        Compliance.Compliant,  # 7
         Compliance.Compliant,  # 8
         Compliance.NotApplicableTest,  # 9
         Compliance.NonCompliant,  # 10
@@ -35,8 +35,8 @@ def test_model_validation_ppm_dynawo_model():
         Compliance.NonCompliant,  # 13
         Compliance.Compliant,  # 14
         Compliance.Compliant,  # 15
-        Compliance.Compliant,  # 16
-        Compliance.Compliant,  # 17
+        Compliance.NotApplicableTest,  # 16
+        Compliance.NotApplicableTest,  # 17
         Compliance.Compliant,  # 18
         Compliance.Compliant,  # 19
         Compliance.NonCompliant,  # 20
@@ -71,10 +71,10 @@ def test_model_validation_bess_dynawo_model():
         Compliance.Compliant,  # 11
         Compliance.Compliant,  # 12
         Compliance.Compliant,  # 13
-        Compliance.NotApplicableTest,  # 14
+        Compliance.Compliant,  # 14
         Compliance.Compliant,  # 15
         Compliance.NotApplicableTest,  # 16
-        Compliance.NotApplicableTest,  # 17
+        Compliance.Compliant,  # 17
         Compliance.Compliant,  # 18
         Compliance.NotApplicableTest,  # 19
         Compliance.NonCompliant,  # 20
@@ -89,20 +89,20 @@ def test_model_validation_bess_dynawo_model():
         Compliance.Compliant,  # 29
         Compliance.Compliant,  # 30
         Compliance.Compliant,  # 31
-        Compliance.Compliant,  # 32
-        Compliance.NonCompliant,  # 33
-        Compliance.Compliant,  # 34
-        Compliance.Compliant,  # 35
+        Compliance.NotApplicableTest,  # 32
+        Compliance.NotApplicableTest,  # 33
+        Compliance.NotApplicableTest,  # 34
+        Compliance.NotApplicableTest,  # 35
         Compliance.Compliant,  # 36
         Compliance.Compliant,  # 37
         Compliance.Compliant,  # 38
         Compliance.Compliant,  # 39
-        Compliance.NonCompliant,  # 40
+        Compliance.Compliant,  # 40
         Compliance.Compliant,  # 41
         Compliance.Compliant,  # 42
         Compliance.Compliant,  # 43
         Compliance.NonCompliant,  # 44
         Compliance.NonCompliant,  # 45
         Compliance.NonCompliant,  # 46
-        Compliance.InvalidTest,  # 47
+        Compliance.Compliant,  # 47
     ] == compliance
